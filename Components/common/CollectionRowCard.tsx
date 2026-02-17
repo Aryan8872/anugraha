d2f2e465287fe68e0b1e65ae813ad17f625961b9
Aryan8872 <gamersimpact778@gmail.com>
2026-02-18 05:20:34 +0545

    fixec collection page responsiveness

--- Components/common/CollectionRowCard.tsx
+++ Components/common/CollectionRowCard.tsx
@@ -2,36 +2,39 @@ import Image from "next/image";
 
 export default function CollectionRowCard() {
   return (
-    <div className="w-full h-[55dvh] grid grid-cols-12">
-      <div className="col-span-8 grid grid-cols-7 gap-x-4">
-        <div className="col-span-5 shadow-sm border border-gray-200 relative">
-          <Image
-            src={"/collection.webp"}
-            alt="collection1"
-            fill
-            className="object-cover"
-          />
-        </div>
-        <div className="relative border border-gray-200 shadow-sm self-end col-span-2 h-[60%]">
-          <Image
-            src={"/collection.webp"}
-            alt="collection1"
-            fill
-            className="object-cover "
-          />
-        </div>
-      </div>
-      <div className="relative col-span-4">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-          dolores illo, iusto nobis corporis cupiditate vitae quaerat
-          temporibus, consequatur vel aliquid quibusdam quasi omnis hic porro,
-          consectetur libero est assumenda.
-        </p>
+    <div className="w-full flex flex-col gap-4">
+      <span className=" font-semibold text-2xl uppercase ">Collection 1</span>
 
-        <button className="absolute bottom-4 right-4 bg-green-primary px-3 py-2 font-medium">
-          view more{" "}
-        </button>
+      <div className="w-full h-[80dvh] md:h-[55dvh] grid grid-cols-1 gap-y-4 grid-rows-[2fr_1fr] md:grid-rows-none md:grid-cols-12">
+        <div className="col-span-8 grid sm:grid-cols-7 gap-x-4   md:h-auto">
+          <div className="col-span-5 shadow-sm border border-gray-200 relative">
+            <Image
+              src={"/collection.webp"}
+              alt="collection1"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="sm:block hidden relative border border-gray-200 shadow-sm self-end col-span-2 h-[60%]">
+            <Image
+              src={"/collection.webp"}
+              alt="collection1"
+              fill
+              className="object-cover "
+            />
+          </div>
+        </div>
+        <div className="relative md:col-span-4">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+            dolores illo, iusto nobis corporis cupiditate vitae quaerat
+            temporibus, consequatur vel aliquid quibusdam quasi omnis hic porro,
+            consectetur libero est assumenda.
+          </p>
+          <button className="absolute bottom-0 right-4 bg-text-black text-[#fefefe] px-3 py-2 font-medium">
+            view more{" "}
+          </button>
+        </div>
       </div>
     </div>
   );
--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -4,24 +4,29 @@ import Image from "next/image";
 export default function CollectionsPage() {
   return (
     <div className="flex w-full flex-col text-text-black  overflow-hidden">
-      <div className="h-screen flex flex-col gap-2">
+      <div className="h-screen flex flex-col gap-5 md:gap-2">
         <h1 className="font-clash-display text-center text-[12vw] sm:text-[10vw] font-medium uppercase leading-none tracking-wider md:text-[11vw]">
           Collections
         </h1>
-        <div className="relative w-[95%] h-[60%] aspect-square ">
+        <p className="md:hidden  block font-clash-display  max-w-[30ch]">
+          Collection that symbolizes Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Distinctio, sint. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Libero, cum?
+        </p>
+        <div className="relative w-[95%] h-[52%] md:h-[60%] aspect-square ">
           <Image
             src={"/collection.webp"}
             alt="collection1"
             fill
             className="object-contain "
           />
-          <p className="absolute top-12 md:left-16 font-clash-display  max-w-[30ch]">
+          <p className="md:block hidden absolute top-12 lg:left-16  font-clash-display  max-w-[30ch]">
             Collection that symbolizes Lorem ipsum dolor sit amet consectetur
             adipisicing elit. Distinctio, sint. Lorem, ipsum dolor sit amet
             consectetur adipisicing elit. Libero, cum?
           </p>
 
-          <span className=" absolute -bottom-5 left-16 uppercase font-clash-display font-semibold text-2xl">
+          <span className=" absolute -bottom-10 md:-bottom-7  uppercase font-clash-display font-semibold text-2xl">
             {"New beginning"}
           </span>
         </div>
